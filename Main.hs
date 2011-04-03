c99572c21bb43d6b27256ddc564f2337af9f60a8
Gabriel Martinez <reitaka@gmail.com>
2011-04-03 18:24:18 -0400

    Added initial printing of board to terminal

--- Main.hs
+++ Main.hs
@@ -9,9 +9,11 @@ import Control.Monad.State
 import Data.Maybe (fromJust)
 import Prelude hiding (lookup)
 import System.Console.CmdArgs
+import Text.PrettyPrint.ANSI.Leijen
 
 type Loc = (Int,Int)
 type St s = State (GOL s)
+type Board s = M.Map Loc s
 
 data Options = Options
   {
@@ -25,24 +27,30 @@ options = Options
 
 data GOL s = GOL
   {
-    board :: M.Map Loc s
+    numRows :: Int
+  , numCols :: Int
+  , board :: Board s
   , vfunc :: ([s] -> s)
   , neighbours :: (Loc -> [Loc])
   }
 
 sample = GOL
   {
-    board = M.fromList [((x,y),1) | x<-[1..10], y<-[1..10]]
+    numRows = 50
+  , numCols = 50
+  , board = M.fromList [if x == 5 then ((x,y),1) else ((x,y),0) | x<-[0..49], y<-[0..49]]
   , vfunc = sum
   , neighbours = \l -> [(1,1), l]
   }
 
 main = do
   options <- cmdArgs options
-  print (run 2)
+  let st = (run 2)
+  save (board st) "temp"
+  print (display (board st) 50 50)
 
 --run :: [(Loc, Int)]
-run n = evalState (sequence_ (replicate n compute) >> lookup (1,1)) sample
+run n = execState (sequence_ (replicate n compute) >> lookup (1,1)) sample
 
 lookup :: Loc -> St s s
 lookup loc = gets (fromJust.M.lookup loc.board)
@@ -61,4 +69,15 @@ compute = do
     vals <- mapM (\k -> liftM (k,) $ newCellVal k) ks
     modify (\st -> st{board=M.fromList vals})
 
---newcellval = getN loc >>= \ns -> mapM lookup ns >>= \vals -> get >>= \st -> 
+save :: Show s => Board s -> FilePath -> IO ()
+save board filename = writeFile filename (show.M.toList $ board)
+
+display :: Board Int -> Int -> Int -> Doc
+display board nrows ncols = vsep $ map (hsep.map format) tbl
+    where tbl = chunk ncols.map snd.M.toList $ board
+          format 0 = text "."
+          format _ = green $ text "O"
+
+chunk _ [] = []
+chunk n xs = let (lhs,rhs) = splitAt n xs
+             in lhs : chunk n rhs
