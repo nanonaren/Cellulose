6d3ef076b8a0abe2590f1cfe3937dddf3a2f2655
Naren Sundaravaradan <nano.naren@gmail.com>
2011-04-03 16:14:20 -0400

    Adding command line framework and cabal build file.

--- Main.hs
+++ Main.hs
@@ -1,14 +1,27 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main
     (
+      main
     ) where
 
 import qualified Data.Map as M
 import Control.Monad.State
 import Data.Maybe (fromJust)
 import Prelude hiding (lookup)
+import System.Console.CmdArgs
 
 type Loc = (Int,Int)
 type St s = State (GOL s)
+
+data Options = Options
+  {
+  } deriving (Data,Typeable)
+
+options = Options
+  {
+  } &= program "gol"
+    &= summary "Game of Life"
+    &= details ["http://github.com/nanonaren/Cellulose"]
 
 data GOL s = GOL
   {
@@ -24,9 +37,12 @@ sample = GOL
   , neighbours = undefined
   }
 
+main = do
+  options <- cmdArgs options
+  putStrLn "hello"
+
 run :: Loc -> Int
 run loc = evalState (lookup loc) sample
 
 lookup :: Loc -> St s s
 lookup loc = gets (fromJust.M.lookup loc.board)
-
