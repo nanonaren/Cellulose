c090771f651164220b3a81568d32dd3ab5bebf17
Naren Sundaravaradan <nano.naren@gmail.com>
2011-04-04 14:58:44 -0400

    In place display updating

--- Main.hs
+++ Main.hs
@@ -9,6 +9,8 @@ import Control.Monad.State
 import Data.Maybe (fromJust)
 import Prelude hiding (lookup)
 import System.Console.CmdArgs
+import System.Console.ANSI
+import System.Process (system)
 import Text.PrettyPrint.ANSI.Leijen
 
 type Loc = (Int,Int)
@@ -34,23 +36,35 @@ data GOL s = GOL
   , neighbours :: (Loc -> [Loc])
   }
 
+sRows = 10
+sCols = 10
 sample = GOL
   {
-    numRows = 50
-  , numCols = 50
-  , board = M.fromList [if x == 5 then ((x,y),1) else ((x,y),0) | x<-[0..49], y<-[0..49]]
-  , vfunc = sum
-  , neighbours = \l -> [(1,1), l]
+    numRows = sRows
+  , numCols = sCols
+  , board = M.fromList [if x == 5 then ((x,y),1) else ((x,y),0) | x<-[0..sRows-1], y<-[0..sCols-1]]
+  , vfunc = flip mod 2.sum
+  , neighbours = \(x,y) -> [(x,y),(x,mod (y+1) sCols),(x,mod (y-1) sCols),
+                            (mod (x+1) sRows,y),(mod (x-1) sRows, y)]
   }
 
 main = do
   options <- cmdArgs options
-  let st = (run 2)
-  save (board st) "temp"
-  print (display (board st) 50 50)
+  display 0 (board sample) sRows sCols
+  run 20
 
---run :: [(Loc, Int)]
-run n = execState (sequence_ (replicate n compute) >> lookup (1,1)) sample
+run n = foldM_ f sample [1..n]
+    where f s i = do
+            let s' = execState compute s
+            clear (numRows s') (numCols s')
+            display i (board s') (numRows s') (numCols s')
+            system "sleep 0.5"
+            return s'
+
+display i brd nrows ncols = print $ createDoc i brd nrows ncols
+clear nrows ncols = do
+  cursorUp (nrows+1)
+  cursorBackward (ncols*2 -1)
 
 lookup :: Loc -> St s s
 lookup loc = gets (fromJust.M.lookup loc.board)
@@ -72,11 +86,13 @@ compute = do
 save :: Show s => Board s -> FilePath -> IO ()
 save board filename = writeFile filename (show.M.toList $ board)
 
-display :: Board Int -> Int -> Int -> Doc
-display board nrows ncols = vsep $ map (hsep.map format) tbl
+createDoc :: Int -> Board Int -> Int -> Int -> Doc
+createDoc iter board nrows ncols =
+    (header <$>).vsep.map (hsep.map format) $ tbl
     where tbl = chunk ncols.map snd.M.toList $ board
           format 0 = text "."
-          format _ = green $ text "O"
+          format _ = green.text $ "O"
+          header = text "Iteration:" <+> int iter
 
 chunk _ [] = []
 chunk n xs = let (lhs,rhs) = splitAt n xs
